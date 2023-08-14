26cc054eba500b3d39fe7d7959edc56f074f8868
Jonathan Zeray <jonathan.zeray@hotmail.com>
2023-08-14 13:30:45 +0200

    .

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -61,7 +61,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
       >
         #{post.tag}
       </p>
-      {session?.user.id === post.creator._id && pathName === "/profile" && (
+      {session?.user.id === post.creator?._id && pathName === "/profile" && (
         <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
           <p
             className="font-inter text-sm green_gradient cursor-pointer"
