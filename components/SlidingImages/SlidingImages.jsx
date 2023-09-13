b5dbf2ddf89c18a4b5e0c15fb743fbfe77af114b
Jonathan Zeray <jonathan.zeray@hotmail.com>
2023-09-13 13:09:35 +0200

    adjusted horiz scroll

--- components/SlidingImages/SlidingImages.jsx
+++ components/SlidingImages/SlidingImages.jsx
@@ -67,8 +67,8 @@ const SlidingImages = () => {
     offset: ["start end", "end start"],
   });
 
-  const x1 = useTransform(scrollYProgress, [0, 1], [0, 125]);
-  const x2 = useTransform(scrollYProgress, [0, 1], [0, -125]);
+  const x1 = useTransform(scrollYProgress, [0, 1], [0, 75]);
+  const x2 = useTransform(scrollYProgress, [0, 1], [0, -75]);
   const height = useTransform(scrollYProgress, [0, 0.9], [50, 0]);
 
   return (
